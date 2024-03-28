b7d7d6c5be07e36ba786323b68f47fd746e86396
SomyaAggarwal1209 <117525704+SomyaAggarwal1209@users.noreply.github.com>
2024-03-28 13:42:32 +0530

    Update window title in main.py

--- main.py
+++ main.py
@@ -65,7 +65,7 @@ def count_down(count):
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
-window.title("Pomodoro")
+window.title("Pomodoro Timer")
 window.config(padx=100, pady=50, bg=YELLOW)
 
 title = Label(text="TIMER", fg=GREEN, font=(FONT_NAME, 40), bg=YELLOW)
